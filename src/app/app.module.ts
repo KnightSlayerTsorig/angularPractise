de9e530aabcf52ef4fba0f83b0500179762f07cb
Oleg Pilyak <olkooop@gmail.com>
2021-01-30 14:53:23 +0200

    Done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,40 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
-import { UsersComponent } from './components/users/users.component';
-import {RouterModule, Routes} from '@angular/router';
 import {HttpClientModule} from '@angular/common/http';
-import { PostComponent } from './components/post/post.component';
-import { PostsComponent } from './components/posts/posts.component';
-import {PostResolveService} from './services/post-resolve.service';
-import { FullPostComponent } from './components/full-post/full-post.component';
+import {RouterModule} from '@angular/router';
 
-const routes: Routes = [
-  {path: 'users', component: UsersComponent},
-  {
-    path: 'posts', component: PostsComponent, resolve: {postsData: PostResolveService}, children: [
-      {path: ':id', component: FullPostComponent}
-    ]
-  }
-];
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UserComponent,
-    UsersComponent,
-    PostComponent,
-    PostsComponent,
-    FullPostComponent
+    AppComponent
   ],
   imports: [
     HttpClientModule,
     BrowserModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot([{
+      path: 'posts', loadChildren: () => import('./post/post.module').then(m => m.PostModule)
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
